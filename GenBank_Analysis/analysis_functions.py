5b2ca4943e2eb0bb88b030f2cf76418f4e2dcbaa
Ghassan Abboud <ghassan.abboud@epfl.ch>
2023-11-03 16:59:26 +0100

    function bodies

---
+++ GenBank_Analysis/analysis_functions.py
@@ -0,0 +1,14 @@
+import Entrez
+import os
+import Biopython
+
+
+def extract_database(seqlen_start = -1, seqlen_stop = -1):
+
+    #return db
+
+def calculate_avg_length(db):
+    #return avg_length
+
+def calculate_unverified_percentage(db):
+
