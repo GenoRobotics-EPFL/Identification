fc6308d7d7ec1b4af1694da8114be56efaa6985f
Ghassan Abboud <ghassan.abboud@epfl.ch>
2023-11-27 01:54:24 +0100

    docstrings + fix download_database for more than 20 results

--- GenBank_Analysis/analysis_functions.py
+++ GenBank_Analysis/analysis_functions.py
@@ -15,7 +15,7 @@ Entrez.email = 'ghassan.abboud@epfl.ch'
 
 def build_search_term(gene_name, min_len = 0, max_len= -1):
     """ generate a search query for NCBI's nucleotide database (GenBank). Bio.Entrez does not provide filters such as sequence length and gene name
-    so a long search query must be used instead.
+    so search query keywords must be used instead.
     Used to make database queries in extract_database and download_database
     
     Parameters
@@ -30,13 +30,9 @@ def build_search_term(gene_name, min_len = 0, max_len= -1):
     """
     term = f"{gene_name}[Gene Name]"
     if max_len == -1:
-        for i in range(min_len):
-            term += f" NOT {i}[Sequence Length]"
+        term += f" NOT 0:{min_len}[Sequence Length]"
     else:
-        term += f" AND ({min_len}[Sequence Length]"
-        for i in range(min_len +1, max_len+1):
-            term += f" OR {i}[Sequence Length]"
-        term += ")"
+        term += f" AND {min_len}:{max_len}[Sequence Length]"
     return term
 
 def extract_database(gene_name, seqlen_start = 0, seqlen_stop = -1):
@@ -52,9 +48,16 @@ def extract_database(gene_name, seqlen_start = 0, seqlen_stop = -1):
     ----------
         seq_record: (str) raw data of all sequences
     '''
-    handle = Entrez.esearch(db= "nucleotide", term= build_search_term(gene_name, seqlen_start, seqlen_stop))
+    handle = Entrez.esearch(db= "nucleotide", term= build_search_term(gene_name, seqlen_start, seqlen_stop), retmax = 10000)
     record = Entrez.read(handle)
     matching_requests = record["IdList"]
+    request_counter = 0
+    while len(record["IdList"]) == 10000:
+        request_counter += 1
+        handle = Entrez.esearch(db= "nucleotide", term= build_search_term(gene_name, seqlen_start, seqlen_stop)
+                                , retstart = request_counter*10000,  retmax = 10000)
+        record = Entrez.read(handle)
+        matching_requests += record["IdList"]
     seq_handle = Entrez.efetch(db="nucleotide", id=matching_requests, retmode = "fasta", rettype = "fasta")
     seq_record = seq_handle.read()
     return seq_record
@@ -97,7 +100,7 @@ def parse_data(path):
     for record in sequences:
         sequence_data.append({'ID': record.id,
                               'Sequence': str(record.seq),
-                              'Description': record.description})
+                              'Description': record.description.lower()})
 
     # Transform the list of dictionnaries to a pandas DataFrame
     df = pd.DataFrame(sequence_data)
@@ -109,12 +112,23 @@ def parse_data(path):
 #-------------------------------
 
 def calculate_unverified_percentage(path_to_fasta):
+    """calculate the percentage of sequences in a fasta file that are labeled 'unverified' in their
+    description, this indicates that they were not checked through the translation to protein.
+    
+    Parameters
+    ----------
+    path_to_fasta: (str) path to the fasta file
+
+    Returns
+    ----------
+    unverified_percentage: (float) percentage of unverfied sequences
+    """
     seq_objects = SeqIO.parse(path_to_fasta, "fasta")  # converts to a seq object
     gene_amount = 0
     unverified_number = 0
     for genes in seq_objects:
         gene_amount += 1
-        if "UNVERIFIED" in genes.description:
+        if "unverified" in genes.description.lower():
             unverified_number += 1
 
     unverified_percentage = unverified_number / gene_amount * 100
@@ -126,11 +140,11 @@ def show_length_graph(path_to_fasta):
     # takes the path to the fasta file as a parameter
     seq_objects = SeqIO.parse(path_to_fasta, "fasta")  # converts to a seq object
     sizes_and_recurrence = {}
-    for genes in seq_objects:  # creating a dict with key:size and value:recurrence amount
-        if len(genes) in sizes_and_recurrence:
-            sizes_and_recurrence[len(genes)] += 1
+    for gene in seq_objects:  # creating a dict with key:size and value:recurrence amount
+        if len(gene.seq) in sizes_and_recurrence:
+            sizes_and_recurrence[len(gene.seq)] += 1
         else:
-            sizes_and_recurrence[len(genes)] = 1
+            sizes_and_recurrence[len(gene.seq)] = 1
     sizes_and_recurrence = sorted([(k, v) for k, v in sizes_and_recurrence.items()])  # sorting the data
     sizes = [k for k, v in sizes_and_recurrence]  # sizes
     recurrences = [v for k, v in sizes_and_recurrence]  # recurrences
@@ -138,8 +152,8 @@ def show_length_graph(path_to_fasta):
     fig, ax = plt.subplots()
     ax.bar(sizes, recurrences)
     ax.set_title('Distribution of Lengths of Genes')
-    plt.xlabel("Lengths of Genes")
-    plt.ylabel("Amount of Genes")
+    plt.xlabel("Length of Gene")
+    plt.ylabel("number of GenBank entries")
     plt.show()
 
 #show_length_graph("/Users/ilgazarslan/Documents/Genorobotics/Identification/Database/matk.fasta")
@@ -162,6 +176,17 @@ def length_grapher_histogram(path_to_fasta):
 
 
 def calculate_avg_length(path_to_fasta):
+    """
+    calculate average length of the sequences in a fasta file
+
+    Parameters
+    ----------
+        path_to_fasta: (str) path of the fasta file
+
+    Returns
+    ----------
+        average: (float) average length in bp
+    """
     seq_objects = SeqIO.parse(path_to_fasta, "fasta")
     number_of_genes = 0
     total_length = 0
@@ -172,6 +197,15 @@ def calculate_avg_length(path_to_fasta):
     return average
 
 def nucleotide_ratio(df):
+    """ calculate the ratio of the four nucleotides for each sequence of a given DataFrame and add the ratios as
+    additional columns of the Dataframe
+     
+    Parameters
+    ----------
+        df: gene DataFrame
+    Returns
+    ----------
+        Nucleotide ration of all sequences """
     df['A'] = df.Sequence.apply(lambda x: x.count('A')/len(x)*100)
     df['T'] = df.Sequence.apply(lambda x: x.count('T')/len(x)*100)
     df['G'] = df.Sequence.apply(lambda x: x.count('G')/len(x)*100)
